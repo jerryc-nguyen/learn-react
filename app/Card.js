b35a24c4f650cac0bb0a112a2a9480d0bac89226
Nhan Nguyen <nhannv.fu6b@gmail.com>
2016-06-28 03:30:19 +0700

    Add lib to convert markdown to HTML

--- app/Card.js
+++ app/Card.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import CheckList from './CheckList'
+import marked from 'marked';
 
 class Card extends Component {
 
   constructor() {
-    super(...arguments)
+    super()
     this.state = {
       showDetails: true
     }
+  }
+
+  toggleDetails() {
+    this.setState({ showDetails: !this.state.showDetails })
   }
 
   render() {
@@ -15,17 +20,30 @@ class Card extends Component {
     if (this.state.showDetails) {
       cardDetails = (
         <div className="card__details">
-          {this.props.description}
+          <span dangerouslySetInnerHTML={{ __html: marked(this.props.description) }} />
           <CheckList cardId={this.props.id} tasks={this.props.tasks} />
         </div>
       )
     }
 
+    let cardClassName = this.state.showDetails ? "card__title card_title--is-open" : "card__title"
+
+    let sideColor = {
+      position: 'absolute',
+      zIndex: -1,
+      top: 0,
+      bottom: 0,
+      left: 0,
+      width: 7,
+      backgroundColor: this.props.color
+    };
+
     return (
       <div className="card">
-        <div className="card__title" onClick={
-          () => this.setState( { showDetails: !this.state.showDetails } )
-        }>{this.props.title}</div>
+        <div style={sideColor}/>
+        <div className={ cardClassName } onClick={ this.toggleDetails.bind(this) }>
+          {this.props.title}
+        </div>
         {cardDetails}
       </div>
     )
--- app/app.js
+++ app/app.js
@@ -6,15 +6,17 @@ let cardsList = [
   {
     id: 1,
     title: "Read the Book",
-    description: "I should read the whole book",
+    description: "I should read the **whole** book",
     status: "in-progress",
+    color: '#BD8D31',
     tasks: []
   },
   {
     id: 2,
     title: "Write some code",
-    description: "Code along with the samples in the book",
+    description: "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)",
     status: "todo",
+    color: '#3A7E28',
     tasks: [
       {
         id: 1,
